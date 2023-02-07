ac4fa5bdbccd5f46843119b7af1f47a94f6e7ee0
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-02-07 13:29:03 +0100

    Publisher crud progressing

--- src/main/java/es/iesrafaelalberti/boardgameApi/controllers/PublisherController.java
+++ src/main/java/es/iesrafaelalberti/boardgameApi/controllers/PublisherController.java
@@ -1,7 +1,28 @@
 package es.iesrafaelalberti.boardgameApi.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import es.iesrafaelalberti.boardgameApi.models.Publisher;
+import es.iesrafaelalberti.boardgameApi.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PublisherController {
-}
+    @Autowired
+    PublisherRepository publisherRepository;
+
+    @GetMapping("/publishers/")
+    public ResponseEntity<Object> index(){ return new ResponseEntity<>(publisherRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/publishers/{id}")
+    public ResponseEntity<Object> show(@PathVariable("id")Long id){
+        return new ResponseEntity<>(publisherRepository.findById(id),HttpStatus.OK);
+    }
+    @PostMapping("/publishers/create")
+    public ResponseEntity<Object> create(@RequestBody Publisher publisher){
+        publisherRepository.save(publisher);
+        return new ResponseEntity<>(publisher,HttpStatus.OK);
+        }
+    }
