27debd098b7230606eaea1e1d3dbc88b597f4889
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-03-07 10:40:00 +0100

    Empezando tests

--- src/test/java/es/iesrafaelalberti/boardgameApi/BoardgameApiApplicationTests.java
+++ src/test/java/es/iesrafaelalberti/boardgameApi/BoardgameApiApplicationTests.java
@@ -1,13 +1,27 @@
 package es.iesrafaelalberti.boardgameApi;
 
+import es.iesrafaelalberti.boardgameApi.repository.BoardgameRepository;
+import es.iesrafaelalberti.boardgameApi.repository.PublisherRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class BoardgameApiApplicationTests {
+	@Autowired
+	MockMvc mvc;
+	@Autowired
+	BoardgameRepository boardgameRepository;
+	@Autowired
+	PublisherRepository publisherRepository;
 
 	@Test
 	void contextLoads() {
+		assert boardgameRepository.count() == 10;
+		assert publisherRepository.count() == 10;
 	}
 
 }
