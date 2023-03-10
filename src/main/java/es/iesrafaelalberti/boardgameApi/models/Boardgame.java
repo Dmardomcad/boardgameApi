18a2c1553905d855e640ac9862cbf7065ce769c9
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-03-10 11:45:46 +0100

    readme

--- src/main/java/es/iesrafaelalberti/boardgameApi/models/Boardgame.java
+++ src/main/java/es/iesrafaelalberti/boardgameApi/models/Boardgame.java
@@ -1,7 +1,7 @@
 package es.iesrafaelalberti.boardgameApi.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,7 +20,7 @@ public class Boardgame {
 
     private String difficulty;
 
-    @JsonIgnoreProperties("id")
+    @JsonManagedReference
     @ManyToOne
     @JoinColumn()
     private Publisher publisher;
