2a0aded8fdf30d521c442669da0ef3a5450407cc
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-03-10 10:17:41 +0100

    Tests con seguridad

--- src/test/java/es/iesrafaelalberti/boardgameApi/BoardgameApiApplicationTests.java
+++ src/test/java/es/iesrafaelalberti/boardgameApi/BoardgameApiApplicationTests.java
@@ -2,13 +2,16 @@ package es.iesrafaelalberti.boardgameApi;
 
 import es.iesrafaelalberti.boardgameApi.repository.BoardgameRepository;
 import es.iesrafaelalberti.boardgameApi.repository.PublisherRepository;
+import es.iesrafaelalberti.boardgameApi.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -21,7 +24,13 @@ class BoardgameApiApplicationTests {
 	BoardgameRepository boardgameRepository;
 	@Autowired
 	PublisherRepository publisherRepository;
-
+	@Autowired
+	UserRepository userRepository;
+	@Test
+	void rootWhenUnauthenticatedThen401() throws Exception {
+		this.mvc.perform(get("/boardgames/"))
+				.andExpect(status().isUnauthorized());
+	}
 	@Test
 	void contextLoads() {
 		assert boardgameRepository.count() == 10;
@@ -29,16 +38,34 @@ class BoardgameApiApplicationTests {
 	}
 	@Test
 	void listTest() throws Exception {
-		mvc.perform(get("/boardgames/").contentType(MediaType.APPLICATION_JSON))
+		MvcResult result = this.mvc.perform(post("/token")
+				.with(httpBasic("dmardom","pestillo")))
+				.andExpect(status().isOk())
+				.andReturn();
+		String token = result.getResponse().getContentAsString();
+
+
+
+		mvc.perform(get("/boardgames/")
+						.header("Authorization","Bearer " + token)
+						.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
 	}
 	@Test
 	void creationTest() throws Exception {
+		MvcResult result = this.mvc.perform(post("/token")
+						.with(httpBasic("dmardom","pestillo")))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		String token = result.getResponse().getContentAsString();
+
 		long boardgameCount = boardgameRepository.count();
 		// Create test boardgame
 		String testBoardgame = "{\"name\": \"Bang!\", \"description\": \"Juego rápido y sencillo para toda la familia\", \"duration\": 25, \"difficulty\": \"Easy\" }";
 		mvc.perform(post("/boardgames/create")
+						.header("Authorization","Bearer " + token)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(testBoardgame))
 				.andExpect(status().isOk())
@@ -51,7 +78,16 @@ class BoardgameApiApplicationTests {
 	}
 	@Test
 	void detailTest() throws Exception {
-		mvc.perform(get("/boardgames/1").contentType(MediaType.APPLICATION_JSON))
+		MvcResult result = this.mvc.perform(post("/token")
+						.with(httpBasic("dmardom","pestillo")))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		String token = result.getResponse().getContentAsString();
+
+		mvc.perform(get("/boardgames/1")
+						.header("Authorization","Bearer " + token)
+						.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
 				// Need to create a specific boardgame using seeders to complete this test
@@ -61,12 +97,20 @@ class BoardgameApiApplicationTests {
 	}
 	@Test
 	void updateTest() throws Exception {
+		MvcResult result = this.mvc.perform(post("/token")
+						.with(httpBasic("dmardom","pestillo")))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		String token = result.getResponse().getContentAsString();
+
 		// Get test boardgame (method get on url /boardgames/1/)
 		// Create test boardgame
 		String testBoardgame = "{\"name\": \"Bang!\", \"description\": \"Juego rápido y sencillo para toda la familia\", \"duration\": 25, \"difficulty\": \"Easy\" }";
 		// modify attributes
 		// Method put on url /boardgames/1/
 		mvc.perform(put("/boardgames/1/")
+						.header("Authorization","Bearer " + token)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(testBoardgame))
 				.andExpect(status().isOk())
@@ -79,8 +123,17 @@ class BoardgameApiApplicationTests {
 	}
 	 @Test
 	void deleteTest() throws Exception {
+		 MvcResult result = this.mvc.perform(post("/token")
+						 .with(httpBasic("dmardom","pestillo")))
+				 .andExpect(status().isOk())
+				 .andReturn();
+
+		 String token = result.getResponse().getContentAsString();
+
 		long boardgameCount = boardgameRepository.count();
-		mvc.perform(delete("/boardgames/1/").contentType(MediaType.APPLICATION_JSON))
+		mvc.perform(delete("/boardgames/1/")
+						.header("Authorization","Bearer " + token)
+						.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().string("true"));
 		assert boardgameRepository.count() == boardgameCount -1;
